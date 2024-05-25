fad359bf6f998311a9f2336f6cb6c4d782a675fc
naingaunglwin-dev <naingaunglwin.wd@gmail.com>
2024-05-25 19:19:15 +0630

    Add language messages files

---
+++ app/Languages/en/exception.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    // Production
+    '404' => "Sorry! We couldn't find the page you're looking for.",
+    '500' => "Sorry! Unexpected error occur.Please try again.",
+
+    // Development
+    "UrlNotFound"        => "Sorry, Your Request URL %s is not found.",
+    "PageNotFound"       => "Sorry, Page not found %s",
+    "ControllerNotFound" => "Sorry, Controller not found",
+    "ModelNotFound"      => "Sorry, Model not found",
+];
